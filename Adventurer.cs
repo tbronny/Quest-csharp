59e6de0492acd7cc844de5a9f3a22aefb85b5833
tbronny <tristan.bron7@gmail.com>
2021-08-27 14:46:57 -0500

    phase 2 complete, user adds name

--- Adventurer.cs
+++ Adventurer.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Quest
 {
     // An instance of the Adventurer class is an object that will undergo some challenges
@@ -6,16 +8,16 @@ namespace Quest
         // This is an "immutable" property. It only has a "get".
         // The only place the Name can be set is in the Adventurer constructor
         // Note: the constructor is defined below.
-        public string Name { get; }
+        public string Name { get; set; }
 
         // This is a mutable property it has a "get" and a "set"
         //  So it can be read and changed by any code in the application
         public int Awesomeness { get; set; }
 
         // A constructor to make a new Adventurer object with a given name
-        public Adventurer(string name)
+        public Adventurer()
         {
-            Name = name;
+            Name = Console.ReadLine();
             Awesomeness = 50;
         }
 
